a95996419a59513f0cb3fbfd08f5c68f9f132962
Ankit Angra <angraankit@gmail.com>
2020-04-19 14:41:07 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {  Text, View } from 'react-native';
+import {Header} from './src/components';
+
+export default function App() {
+  return (
+    <View>
+      <Header/>
+    </View>
+  );
+}
+
+
